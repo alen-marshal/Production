b344856bcdd07390cb60f7f5ee1a147596871541
alenmarshal202 <alenmarshal202@gmail.com>
2023-02-22 14:10:34 +0530

    update with pkl

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from sklearn.linear_model import LinearRegression
 app = Flask(__name__)
 
 # Load the pickled data and store it in a global variable
-with open('finalized_model.sav', 'rb') as f:
+with open('model_socialnetwork_ad.pkl', 'rb') as f:
     model = pickle.load(f)
 
 
